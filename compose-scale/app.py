236099df76277356dc204a11a3c8f61e5084d8fd
John serrano <andrey_201015@hotmail.com>
2018-06-01 11:38:17 -0500

    Cambios en compose-file

--- compose-scale/app.py
+++ compose-scale/app.py
@@ -2,10 +2,12 @@ import time
 
 import redis
 from flask import Flask
+import socket
 
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
+host = socket.gethostname()
 
 
 def get_hit_count():
@@ -23,7 +25,7 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello from Docker! John Serrano I have been seen {} times.\n'.format(count)
+    return 'Hello from Docker! John Serrano I have been seen {} times. Host name is {} \n'.format(count, host)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
